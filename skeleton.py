763ec5b2dc3cdc0ee8d3e1d3dcd2fef77784aec9
mrb114 <mrb114@case.edu>
2017-04-08 19:47:03 -0700

    Completed implementation of Knn and SVC

--- skeleton.py
+++ skeleton.py
@@ -62,56 +62,55 @@ def clean_separate(datafile):
 
 def ml_fit(training_x, training_y, verification_x, verification_y): 
     # Multi-variable logistic regression 
-    log_C = np.arange(1e-9, 1e-2, 1e-3)
+    log_C = np.arange(1e-3, 1e-1, 1e-3)
     score_vals=[]
     for C in log_C: 
         log_model = sklearn.linear_model.LogisticRegression(solver='newton-cg',C=C, max_iter=100)
         log_model = log_model.fit(training_x.as_matrix(), training_y.as_matrix())
         curr_score = log_model.score(verification_x.as_matrix(), verification_y.as_matrix())
         score_vals.append(curr_score)
-        #print log_model.n_iter_
-        #print curr_score
         
     logr_optimal_C = log_C[np.argmax(score_vals)]
     logr_optimal_score = np.max(score_vals)
-    print "Logistic Regression C = %.2f score = %.4f" %(logr_optimal_C, logr_optimal_score)
+    print "Logistic Regression C = %.4f score = %.4f" %(logr_optimal_C, logr_optimal_score)
     
     # Least Squares Regression 
     lsq_model = sklearn.linear_model.LinearRegression()
     lsq_model = lsq_model.fit(training_x.as_matrix(), training_y.as_matrix())
-    score = lsq_model.score(verification_x.as_matrix(), verification_y.as_matrix())
-    print "Least Squares Regression score = %.4f" %(score)
+    lsq_predicted_y = np.round(lsq_model.predict(verification_x.as_matrix()))
+    curr_score = score(lsq_predicted_y, verification_y.as_matrix())
+    print "Least Squares Regression score = %.10f" %(curr_score)
     
     # Ridge Regression 
-    ridge_alpha = np.arange(1, 10, 10)#0.2)
+    ridge_alpha = np.arange(1e-9, 1e-5, 1e-6)
     score_vals = []
     for alpha in ridge_alpha: 
         ridge_model = sklearn.linear_model.Ridge(alpha=alpha)
         ridge_model = ridge_model.fit(training_x.as_matrix(), training_y.as_matrix())
-        curr_score = ridge_model.score(verification_x.as_matrix(), verification_y.as_matrix())
+        ridge_predicted_y = np.round(ridge_model.predict(verification_x.as_matrix()))
+        curr_score = score(ridge_predicted_y, verification_y.as_matrix())
         score_vals.append(curr_score)
-        #print curr_score
         
     ridge_optimal_alpha = ridge_alpha[np.argmax(score_vals)]
     ridge_optimal_score = np.max(score_vals)
-    print "Ridge Regression alpha = %.2f score = %.10f" %(ridge_optimal_alpha, ridge_optimal_score)
+    print "Ridge Regression alpha = %.10f score = %.10f" %(ridge_optimal_alpha, ridge_optimal_score)
     
     # LASSO 
-    lasso_alpha = np.arange(0.0001, 0.1, 1) #0.001)
+    lasso_alpha = np.arange(0.0001, 0.1, 0.001)
     score_vals = []
     for alpha in lasso_alpha: 
         lasso_model = sklearn.linear_model.Lasso(alpha=alpha)
         lasso_model = lasso_model.fit(training_x.as_matrix(), training_y.as_matrix())
-        curr_score = lasso_model.score(verification_x.as_matrix(), verification_y.as_matrix())
+        lasso_predicted_y = np.round(lasso_model.predict(verification_x.as_matrix()))
+        curr_score = score(lasso_predicted_y, verification_y.as_matrix())
         score_vals.append(curr_score)
-        #print curr_score
     
     lasso_optimal_alpha = lasso_alpha[np.argmax(score_vals)]
     lasso_optimal_score = np.max(score_vals)
-    print "LASSO alpha = %.2f score = %.10f" %(lasso_optimal_alpha, lasso_optimal_score)
+    print "LASSO alpha = %.4f score = %.10f" %(lasso_optimal_alpha, lasso_optimal_score)
     
     # Knn
-    knn_k = np.arange(2, 50, 2)
+    knn_k = np.arange(7, 20, 1)
     score_vals = []
     for k in knn_k: 
         knn_model = sklearn.neighbors.KNeighborsClassifier(n_neighbors=k)
@@ -122,44 +121,25 @@ def ml_fit(training_x, training_y, verification_x, verification_y):
     knn_optimal_k = knn_k[np.argmax(score_vals)]
     knn_optimal_score = np.max(score_vals)
     print "Knn k = %d score = %.10f" %(knn_optimal_k, knn_optimal_score)
-    
-    # SVR
-    svr_c = np.arange(0.1, 2, 0.5)
-    svr_epsilon = np.arange(0.1, 2, 0.5)
-    score_vals = []
-    for c in svr_c: 
-        for epsilon in svr_epsilon: 
-            svr_model = sklearn.svm.SVR(C=c, epsilon=epsilon)
-            svr_model = svr_model.fit(training_x.as_matrix(), training_y.as_matrix())
-            curr_score = svr_model.score(verification_x.as_matrix(), verification_y.as_matrix())
-            score_vals.append(curr_score)
-            print curr_score
-    
-    svr_optimal_c = svr_c[np.argmax(score_vals)]
-    svr_optimal_epsilon = svr_epsilon[np.argmax(score_vals)]
-    svr_optimal_score = np.max(score_vals)
-    print "SVR c = %.3f epsilon = %.3f score = %.10f" %(svr_optimal_c, svr_optimal_epsilon, svr_optimal_score)
-    
+      
     #SVC
-    svc_c = np.arange(0.1, 3, 0.5)
-    svc_gamma = np.arange(0.001, 0.01, 0.001)
+    svc_c = np.arange(-500 , -5, 50)
+    svc_gamma = np.arange(1e-9, 1e-4, 1e-5)
     score_vals = []
     for c in svc_c: 
-        for gamma in svc_gamma: 
-            svc_model = sklearn.svm.SVC(C=c, gamma=gamma)
-            svc_model = svc_model.fit(training_x.as_matrix(), training_y.as_matrix())
-            curr_score = svr_model.score(verification_x.as_matrix(), verification_y.as_matrix())
-            score_vals.append(curr_score)
-            print curr_score
+        svc_model = sklearn.svm.SVC(C=0.001,kernel='poly',degree=1,coef0=1.0)
+        svc_model = svc_model.fit(training_x.as_matrix(), training_y.as_matrix())
+        curr_score = svc_model.score(verification_x.as_matrix(), verification_y.as_matrix())
+        score_vals.append(curr_score)
     
     svc_optimal_c = svc_c[np.argmax(score_vals)]
     svc_optimal_gamma = svc_gamma[np.argmax(score_vals)]
     svc_optimal_score = np.max(score_vals)
-    print "SVC c = %.3f gamma = %.3f score = %.10f" %(svc_optimal_c, svc_optimal_gamma, svc_optimal_score)
+    print "SVC c = %.3f gamma = %.7f score = %.10f" %(svc_optimal_c, svc_optimal_gamma, svc_optimal_score)
     
-    return log_model, lsq_model, ridge_model, lasso_model
+    return log_model, lsq_model, ridge_model, lasso_model, knn_model, svc_model
     
-def ml_predict(testing_x, testing_y, log_model, lsq_model, ridge_model, lasso_model): 
+def ml_predict(testing_x, testing_y, log_model, lsq_model, ridge_model, lasso_model, knn_model, svc_model): 
 
     # Logistic Regression
     log_predicted_y = log_model.predict(testing_x.as_matrix())
@@ -175,8 +155,8 @@ def ml_predict(testing_x, testing_y, log_model, lsq_model, ridge_model, lasso_mo
     plt.show()
     
     # Least Squares Regression 
-    lsq_predicted_y = lsq_model.predict(testing_x.as_matrix())
-    lsq_score = lsq_model.score(testing_x.as_matrix(), testing_y.as_matrix())
+    lsq_predicted_y = np.round(lsq_model.predict(testing_x.as_matrix()))
+    lsq_score = score(lsq_predicted_y,testing_y.as_matrix())
     plt.figure()
     plt.plot(testing_x, testing_y, 'bo', testing_x, lsq_predicted_y, 'rs')
     plt.title('Least Squares Regression')
@@ -185,8 +165,8 @@ def ml_predict(testing_x, testing_y, log_model, lsq_model, ridge_model, lasso_mo
     plt.show()
     
     # Ridge Regression 
-    ridge_predicted_y = ridge_model.predict(testing_x.as_matrix())
-    ridge_score = ridge_model.score(testing_x.as_matrix(), testing_y.as_matrix())
+    ridge_predicted_y = np.round(ridge_model.predict(testing_x.as_matrix()))
+    ridge_score = score(ridge_predicted_y, testing_y.as_matrix())
     plt.figure()
     plt.plot(testing_x, testing_y, 'bo', testing_x, ridge_predicted_y, 'rs')
     plt.title('Ridge Regression')
@@ -195,8 +175,8 @@ def ml_predict(testing_x, testing_y, log_model, lsq_model, ridge_model, lasso_mo
     plt.show()
     
     # Lasso 
-    lasso_predicted_y = lasso_model.predict(testing_x.as_matrix())
-    lasso_score = lasso_model.score(testing_x.as_matrix(), testing_y.as_matrix())
+    lasso_predicted_y = np.round(lasso_model.predict(testing_x.as_matrix()))
+    lasso_score = score(lasso_predicted_y, testing_y.as_matrix())
     plt.figure()
     plt.plot(testing_x, testing_y, 'bo', testing_x, lasso_predicted_y, 'rs')
     plt.title('LASSO Regression')
@@ -205,17 +185,42 @@ def ml_predict(testing_x, testing_y, log_model, lsq_model, ridge_model, lasso_mo
     axes = plt.gca()
     axes.set_ylim([-0.5, 1.5])
     plt.show()
+
+    # K-nn
+    knn_predicted_y = knn_model.predict(testing_x.as_matrix())
+    knn_score = knn_model.score(testing_x.as_matrix(), testing_y.as_matrix())
+    plt.figure()
+    plt.plot(testing_x, testing_y, 'bo', testing_x, knn_predicted_y, 'rs')
+    plt.title('K-nn')
+    plt.xlabel('Feature')
+    plt.ylabel('Predicted and Actual Result')
+    axes = plt.gca()
+    axes.set_ylim([-0.5, 1.5])
+    plt.show()
     
+    # SVC
+    svc_predicted_y = svc_model.predict(testing_x.as_matrix())
+    svc_score = svc_model.score(testing_x.as_matrix(), testing_y.as_matrix())
+    plt.figure()
+    plt.plot(testing_x, testing_y, 'bo', testing_x, svc_predicted_y, 'rs')
+    plt.title('SVC')
+    plt.xlabel('Feature')
+    plt.ylabel('Predicted and Actual Result')
+    axes = plt.gca()
+    axes.set_ylim([-0.5, 1.5])
+    plt.show()
     
     print "Logistic Regression score = %.10f" % log_score
     print "Least Squares Regression score = %.10f" % lsq_score
     print "Ridge Regression score = %.10f" % ridge_score 
     print "LASSO Regression score = %.10f" % lasso_score
+    print "K-nn score = %.10f" % knn_score
+    print "SVC score = %.10f" % svc_score
     
     def find_best_classifier():
-        classifiers = ([log_model, lsq_model, ridge_model, lasso_model])
-        classifier_names = (["logistic regression", "least squares regression", "ridge regression", "LASSO"])
-        scores = ([log_score, lsq_score, ridge_score, lasso_score])
+        classifiers = ([log_model, lsq_model, ridge_model, lasso_model, knn_model, svc_model])
+        classifier_names = (["logistic regression", "least squares regression", "ridge regression", "LASSO", "K-nn", "SVC"])
+        scores = ([log_score, lsq_score, ridge_score, lasso_score, knn_score, svc_score])
         best_classifier = classifiers[np.argmax(scores)]
         print classifier_names[np.argmax(scores)]
         return best_classifier
@@ -235,18 +240,19 @@ def random_forest_feature_importance(X, Y):
     importances = rf.feature_importances_
     plot_feature_importance(importances, X, Y)
     indices = np.argsort(importances)
-    minimum_significance = 0.001    
-    num_features_keeping = 50
-    #most_important = best_ordered_importances[best_ordered_importances > minimum_significance]
+    num_features_keeping = 10
     ordered_importances = importances[indices]
-    #least_important = ordered_importances[ordered_importances < minimum_significance]
-    #least_important_indices = indices[ordered_importances < minimum_significance]
     least_important = ordered_importances[:-num_features_keeping]
     least_important_indices = indices[:-num_features_keeping]
     best_indices = np.argsort(importances)[::-1]
     return least_important_indices, least_important, best_indices
+
+def score(predicted_y, true_y): 
+    correct = np.sum(predicted_y == true_y)
+    return correct / float(len(predicted_y))
     
 # Read in the datafile and clean it
+np.random.seed(6254)
 datafile = 'dataset_diabetes/diabetic_data.csv'
 training_x, training_y, testing_x, testing_y, verification_x, verification_y = clean_separate(datafile)
 
@@ -262,10 +268,15 @@ verification_x = verification_x.drop(list(np.array(feature_list)[indices]), axis
 training_x = training_x.drop(list(np.array(feature_list)[indices]), axis=1)
 
 # do machine learning
-log_model, lsq_model, ridge_model, lasso_model = ml_fit(training_x, training_y, verification_x, verification_y)
-best_classifier = ml_predict(testing_x, testing_y, log_model, lsq_model, ridge_model, lasso_model)
-# TODO:  Add SVM & Knn
-#        Determine the ratio of admittance in testing, verification, and training groups 
+log_model, lsq_model, ridge_model, lasso_model, knn_model, svc_model = ml_fit(training_x, training_y, verification_x, verification_y)
+best_classifier = ml_predict(testing_x, testing_y, log_model, lsq_model, ridge_model, lasso_model ,knn_model, svc_model)
+
+# Determine success of random classifier 
+random_y = np.random.randint(0, 2, size=len(testing_y))
+random_classifier_score = score(random_y, testing_y)
+print "Random Classifier score = %.10f" % random_classifier_score
+
+# TODO:  Determine the ratio of admittance in testing, verification, and training groups 
 #        Randomly assigning predictions to the test group and compare to our best score 
 #        
 #        create plots/visuals of results
